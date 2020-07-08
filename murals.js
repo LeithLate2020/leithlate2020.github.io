154462c8dadf4ce7642c2f37e8e2ed4658fc6921
devilreef <gavin.inglis@gmail.com>
2020-07-08 16:10:44 +0100

    Move mural data to external file

---
+++ murals.js
@@ -0,0 +1,92 @@
+var muralData = {
+  "type" : "FeatureCollection",
+  "features" : [
+    {
+      "type" : "Feature",
+      "properties" : {
+        "name" : "Wronger Rites",
+        "folder" : "WrongerRites",
+        "artist" : "Kirsty Whiten",
+        "description" :
+            "<p>Kirsty Whiten’s Wronger Rites mural was commissioned as part of The Mural Project for the LeithLate15 festival, with associated temporary public art installations on Leith Walk opposite the former Edinburgh Printmakers and an exhibition at the former Whitespace gallery. In 2017 two thirds of the mural were sadly demolished, though the horse figure remains intact.</p><p>Artist website: <a href='https://www.kirstywhiten.com/'>www.kirstywhiten.com</a></p>",
+        "icon" : "museum",
+        "audio" : [
+          {
+            "file" : "CameronFoster.mp3",
+            "transcript" : "CameronFoster.pdf",
+            "label" : "Cameron Foster Audio Guide",
+          },
+          {
+            "file" : "KirstyWhiten.mp3",
+            "transcript" : "KirstyWhiten.pdf",
+            "label" : "Kirsty Whiten on Wronger Rites",
+          },
+        ],
+      },
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [-3.174445359,55.96481864],
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "name" : "Leith Dockers Club Mural",
+        "folder" : "DockersClub",
+        "artist" : "Tom Ewing",
+        "description" :
+            "<p>This mural was painted as a celebration of the history of both Leith and the Leith Dockers Club, and was unveiled by the author Irvine Welsh in early 2014. Although on private grounds, the mural is publicly available to view whenever the Leith Dockers Club is open.</p>",
+        "icon" : "harbor",
+        "audio" : [
+          {
+            "file" : "CameronFoster.mp3",
+            "transcript" : "CameronFoster.pdf",
+            "label" : "Cameron Foster Audio Guide",
+          },
+        ]
+      },
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [-3.170117861,55.97073971],
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "name" : "Leith Aquatic Mural",
+        "folder" : "LeithAquatic",
+        "artist" : "Blameless Collective",
+        "description" :
+            "<p>The Leith Aquatic was the first mural fundraised for and commissioned by LeithLate, as part of The Mural Project in 2013. It was produced by Blameless Collective through local community consultation and was also the first large-scale mural to be installed in Leith for almost three decades, kickstarting a new association with public art in the area. The mural launch in July 2013 included a street party for local residents with speeches, DJs and free daal courtesy of Sikh Sanjog.</p>",
+        "icon" : "aquarium",
+        "audio" : [
+          {
+            "file" : "CameronFoster.mp3",
+            "transcript" : "CameronFoster.pdf",
+            "label" : "Cameron Foster Audio Guide",
+          },
+          {
+            "file" : "RabiyaChoudhry.mp3",
+            "transcript" : "RabiyaChoudhry.pdf",
+            "label" : "Rabiya Choudhry on Leith Aquatic Mural",
+          },
+          {
+            "file" : "RichieCumming.mp3",
+            "transcript" : "RichieCumming.pdf",
+            "label" : "Richie Cumming on Leith Aquatic Mural",
+          },
+          {
+            "file" : "FraserGray.mp3",
+            "transcript" : "FraserGray.pdf",
+            "label" : "Fraser Gray on Leith Aquatic Mural",
+          },
+
+        ],
+      },
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [-3.170161848,55.9678748],
+      }
+    }
+  ]
+}
