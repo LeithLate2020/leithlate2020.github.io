497917d48dd7acc089c0a8a5142c6e9c21f91191
devilreef <gavin.inglis@gmail.com>
2020-07-21 16:48:43 +0100

    Add four more studios

--- studios.js
+++ studios.js
@@ -4,6 +4,50 @@ var studioData = {
     {
       "type" : "Feature",
       "properties" : {
+        "name" : "Edinburgh Palette, St. Margaret&rsquo;s House",
+        "folder" : "StMargarets",
+        "address" : "151 London Road, Edinburgh EH7 6AE",
+        "description" :
+            "<p>Edinburgh Palette is a community of makers, designers and artists, working alongside charities, creative enterprises and small businesses. In St Margaret’s House, the charity has created an inclusive hub where artists, craftspeople, community groups and businesses share space, interact and exchange ideas, while providing a diverse programme of public events and exhibitions.</p><p><a href='https://www.edinburghpalette.co.uk/' target='_blank'>https://www.edinburghpalette.co.uk/</a></p>",
+        "icon" : "studio",
+        "audio" : [
+          {
+            "file" : "CameronFoster.mp3",
+            "transcript" : "CameronFoster.pdf",
+            "label" : "Cameron Foster Audio Guide",
+          },
+        ],
+      },
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [-3.1522477,55.9556382],
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "name" : "Rhubaba Gallery and Studios",
+        "folder" : "Rhubaba",
+        "address" : "25 Arthur Street Edinburgh, EH6 5DA",
+        "description" :
+            "<p>Rhubaba Gallery and Studios is an artist-run organisation in Edinburgh that provides studio space for artists and practitioners across fifteen studios alongside an annual programme of exhibitions and events. They aim to give early-career artists the opportunity to produce new work in a discursive environment, work collaboratively with other artist-led initiatives and explore wider debates in contemporary art, bringing exciting and challenging works to Scotland.</p><p><a href='http://www.rhubaba.org/' target='_blank'>http://www.rhubaba.org/</a></p>",
+        "icon" : "studio",
+        "audio" : [
+          {
+            "file" : "CameronFoster.mp3",
+            "transcript" : "CameronFoster.pdf",
+            "label" : "Cameron Foster Audio Guide",
+          },
+        ],
+      },
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [-3.1777782,55.9649896],
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
         "name" : "OOTB Drill Hall",
         "folder" : "OOTBDrillHall",
         "address" : "36 Dalmeny Street, Edinburgh EH6 8RG",
@@ -43,6 +87,50 @@ var studioData = {
       "geometry" : {
         "type" : "Point",
         "coordinates" : [-3.1724578,55.9568593],
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "name" : "Custom Lane",
+        "folder" : "CustomLane",
+        "address" : "1 Customs Wharf, Edinburgh EH6 6AL",
+        "description" :
+            "<p>Custom Lane is an evolving, collaborative experiment in creating the most engaging, enriching and enjoyable work environment for designers and design-led makers in Scotland. Comprised of a café, shop, gallery, event space and workshop, the ground floor is fully accessible to the public and offers the &lsquo;best coffee in Leith&rsquo;. Upstairs is home to a wide variety of designers, makers and creative industry professionals.</p><p><a href='https://customlane.co/' target='_blank'>https://customlane.co/</a></p>",
+        "icon" : "studio",
+        "audio" : [
+          {
+            "file" : "CameronFoster.mp3",
+            "transcript" : "CameronFoster.pdf",
+            "label" : "Cameron Foster Audio Guide",
+          },
+        ],
+      },
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [-3.1703377,55.9767833],
+      }
+    },
+    {
+      "type" : "Feature",
+      "properties" : {
+        "name" : "Coburg House",
+        "folder" : "CoburgHouse",
+        "address" : "15 Coburg Street, Edinburgh, EH6 6ET",
+        "description" :
+            "<p>Coburg House is a creative home for over 80 artists, makers and designers. This former granary has housed studios for nearly 20 years and is home to a dynamic creative community with a well-established reputation for excellence.  Coburg House offers a range of workshops and classes and has a gallery and shop open to the public every weekend.</p><p><a href='https://www.coburghouse.co.uk/' target='_blank'>https://www.coburghouse.co.uk/</a></p>",
+        "icon" : "studio",
+        "audio" : [
+          {
+            "file" : "CameronFoster.mp3",
+            "transcript" : "CameronFoster.pdf",
+            "label" : "Cameron Foster Audio Guide",
+          },
+        ],
+      },
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [-3.1743597,55.9761289],
       }
     },
   ]
